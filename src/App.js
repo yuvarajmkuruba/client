62fd12882bb034611a52254ac7afe8a434c2566e
Yuvaraj <yuvarajmkuruba@gmail.com>
2023-06-27 17:53:41 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { Route, Routes} from 'react-router-dom'
 import Navbar from './components/Navbar'
-
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import  Register from './components/Register'
 const App = () => {
-  return (
+  return (  
     <>
       <Navbar />
+      
+      {/* <Route path='/' >
+      <Home />
+      </Route> */}
+      {/* <Route path='/'>
+         <Home />
+      </Route>
+
+      <Route path='/About'>
+         <About />
+      </Route>
+      <Route path='/Contact'>
+         <Contact />
+      </Route>
+
+      <Route path='/Register'>
+         <Register />
+      </Route> */}
     </>
   )
 }
